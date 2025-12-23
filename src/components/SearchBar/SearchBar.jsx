03d9d14f5b513e8b5dfe00ee1a8b855d2c7e90f4
Domenico Pagano <dpaganoh@gmail.com>
2025-12-23 11:38:18 -0500

    ajustes en Navbar y Searchbar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -9,9 +9,7 @@ export default function SearchBar(props) {
    const handleChange = (e) => {
       const {value} = e.target;
       setCharacter(value)
-      //console.log('Esto es value', value)
    }
-   //console.log('Esto es character',character)
 
    const cleanInput = () => {
       setCharacter('')
@@ -20,25 +18,24 @@ export default function SearchBar(props) {
    const handleSearch = () => {
       props.onSearch(character);
       inputRef.current.focus();
+      cleanInput()
    }
 
    const handleKeyPress = (e) => {
       if(e.key === 'Enter'){
          e.preventDefault();
          handleSearch();
-         cleanInput()
       }
    }
 
    return (
       <div className={style.container}>
-         <h2 className={style.indicador}>ID:</h2>
          <input 
             ref={inputRef}
             type='search' 
             name='search'
             id='search'
-            placeholder='id here'
+            placeholder='id'
             value={character}
             onChange={handleChange}
             onKeyPress={handleKeyPress}
@@ -47,35 +44,3 @@ export default function SearchBar(props) {
       </div>
    );
 }
-
-/* Funcionamiento Componente SearchBar:
-
-   1. Al hacer algún cambio en el input del SearchBar se ejecutará la función handleChange.
-      handleChange guarda el texto ingresado en el input en la variable 'value' y modifica 
-      el estado del componente (setCharacter) asignándole el valor de 'value'.
-      Ahora el estado del componente (character) es 'value'.
-   
-   2. Al hacer click en el boton se dispara la función 'onSearch' con el estado 'character'
-      como parámetro. Esta función es pasada por props desde 'App.jsx':
-
-         function onSearch(id) {
-            axios(`https://rickandmortyapi.com/api/character/${id}`)
-            .then(
-               ({ data }) => {
-                  if (data.name) {
-                     setCharacters((oldChars) => [...oldChars, data]);
-                  } else {
-                     window.alert('¡No hay personajes con este ID!');
-                  }
-               }
-            );
-         }
-
-      Digamos que hemos escrito el id '10' en el input: onSearch(id) => onSearch(character) => onSearch(10)
-      axios pedirá a la API la información del personaje con id '10'. Cuando obtenga respuesta la manejará
-      con el método .then() preguntándo si dentro de la 'data' obtenida existe la propiedad 'name' (data.name).
-      Si es así es que hubo coincidencia y actualizará el estado añadiendo al nuevo personaje (id:10) a los
-      añadidos previamente.
-      Si no hay coincidencia lanzará un alert: '¡No hay personajes con este ID!'
-
-*/
